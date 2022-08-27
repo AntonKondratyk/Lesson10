dd7e0ecf100d79e987c424128fb099334e6faa07
Anton Kondratyk <andeev1910@mail.ru>
2022-08-27 23:49:16 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Clock.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Angle
+    {
+        double grad; // это поля. наполнили данными
+        double min;
+        double sec;
+
+        public double Grad
+        {
+            get
+            {
+                return grad;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    grad = value;
+                }
+                else
+                {
+                    Console.WriteLine("0-60!!!");
+                };
+            }
+        }
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("0-60!!!");
+                };
+            }
+        }
+        public double Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("0-60!!!");
+                };
+            }
+        }
+        public Angle(double grad, double min, double sec)
+        {
+            this.grad = grad;
+            this.min = min;
+            this.sec = sec;
+        }
+
+        public double ToRadians() // переменные прописывать не надо, метод имеет к ним доступ внутри класса
+        {
+            return (grad+(min + sec/60)/60)*Math.PI/180;
+        }
+    }
+    //class Clock
+    //{
+    //    int hour; // это поля. наполнили данными
+    //    int minute;
+    //    int second;
+
+    //    public int Hour
+    //    {
+    //        get
+    //        {
+    //            return hour;
+    //        }
+    //        set
+    //        {
+    //            hour = value % 24;
+    //        }
+    //    }
+    //    public int Minute
+    //    {
+    //        get
+    //        {
+    //            return minute;
+    //        }
+    //        set
+    //        {
+    //            Hour += value / 60;
+    //            minute = value % 60;
+    //         }
+    //    }
+    //    public int Second
+    //    {
+    //        get
+    //        {
+    //            return second;
+    //        }
+    //        set
+    //        {
+    //            Minute += value /60;
+    //            second = value % 60;
+    //        }
+    //    }
+    //    public Clock(int hour, int minute, int second)
+    //    {
+    //        this.hour = hour;
+    //        this.minute = minute;
+    //        this.second = second;
+    //    }
+
+    //    public int ToSecond() // переменные прописывать не надо, метод имеет к ним доступ внутри класса
+    //    {
+    //        return second + minute * 60 + hour * 60 * 60;
+    //    }
+    //}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grad_ = Convert.ToInt32(Console.ReadLine());
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(grad_, min_, sec_);
+            //
+            //https://planetcalc.ru/71/#:~:text=%D0%9F%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%20%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8%D1%86%D1%8B%20%D0%B8%D0%B7%D0%BC%D0%B5%D1%80%D0%B5%D0%BD%D0%B8%D1%8F%20%D1%83%D0%B3%D0%BB%D0%BE%D0%B2%20%D0%B8%D0%B7,%D1%81%D0%B5%D0%BA%D1%83%D0%BD%D0%B4%D1%8B%2F60)%2F60%C2%BB.
+            //
+            double totalSecond = angle.ToRadians();
+            Console.WriteLine("{0:f4}",totalSecond);
+            Console.ReadKey();
+            //int hour_ = Convert.ToInt32(Console.ReadLine());
+            //int minute_ = Convert.ToInt32(Console.ReadLine());
+            //int second_ = Convert.ToInt32(Console.ReadLine());
+
+            //Clock clock = new Clock(hour_, minute_, second_);
+
+            ////Clock clock = new Clock() {hour=hour_, minute=minute_,second=second_};
+            ////clock.hour = Convert.ToInt32(Console.ReadLine());
+            ////clock.minute = Convert.ToInt32(Console.ReadLine());
+            ////clock.second = Convert.ToInt32(Console.ReadLine());
+
+
+            //int totalSecond = clock.ToSecond();
+            //Console.WriteLine(totalSecond);
+            //Console.ReadKey();
+        }
+    }
+}
