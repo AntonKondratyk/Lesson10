d89a07071ecc2fd8d5cf8ccecb7bc7275a0d55da
Anton Kondratyk <andeev1910@mail.ru>
2022-08-28 16:56:30 +0500

    15. Интерфейсы

---
+++ App7/ISeries.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App7
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArPr : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeoPr : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+
+}
---
+++ App7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArPr arpr = new ArPr();
+            arpr.SetStart(10);
+            arpr.setStep(2);
+            Console.WriteLine(arpr.GetNext());
+            Console.WriteLine(arpr.GetNext());
+            Console.WriteLine(arpr.GetNext());
+            arpr.Reset();
+            Console.WriteLine(arpr.GetNext());
+            Console.WriteLine(arpr.GetNext());
+            GeoPr geopr = new GeoPr();
+            geopr.SetStart(10);
+            geopr.setStep(2);
+            Console.WriteLine(geopr.GetNext());
+            Console.WriteLine(geopr.GetNext());
+            Console.WriteLine(geopr.GetNext());
+            geopr.Reset();
+            Console.WriteLine(geopr.GetNext());
+            Console.WriteLine(geopr.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
