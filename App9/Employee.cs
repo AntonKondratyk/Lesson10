28a1f5221722699686cfb908486771e474514ba2
Anton Kondratyk <andeev1910@mail.ru>
2022-08-28 20:32:05 +0500

    САМОСТОЯТЕЛЬНАЯ РАБОТА 17. ОБОБЩЕННЫЕ ТИПЫ (GENERICS)

---
+++ App9/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App9
+{
+    class Acc<T>
+    {
+        private T Kod { get; set; }
+        private string Name { get; set; }
+        private int Price { get; set; }
+
+
+        public string GetInfo()
+        {
+            return $"{Kod} {Name} {Price}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Kod =(T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Price = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        //public Tovar(T kod, string name, int price)
+        //{
+        //    Kod = kod;
+        //    Name = name;
+        //    Price = price;
+        //}
+
+        //class Tovar<T>
+        //{
+        //    public T Kod { get; set; }
+        //    public string Name { get; set; }
+        //    public int Price { get; set; }
+
+        //    //public Tovar(T kod, string name, int price)
+        //    //{
+        //    //    Kod = kod;
+        //    //    Name = name;
+        //    //    Price = price;
+        //    //}
+
+        //    public string GetInfo()
+        //    {
+        //        return $"{Kod} {Name} {Price}";
+        //    }
+
+        //    public void Input()
+        //    {
+        //        Console.WriteLine("Введите номер");
+        //        Kod = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+        //        Console.WriteLine("Введите имя");
+        //        Name = Console.ReadLine();
+        //        Console.WriteLine("Введите цену");
+        //        Price = Convert.ToInt32(Console.ReadLine());
+        //    }
+
+
+
+    }
+    //class Employee
+    //{
+    //    public int Num { get; set; }
+    //    public string Name { get; set; }
+    //    public int Summa { get; set; }
+    //}
+}
---
+++ App9/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Acc<string> acc1 = new Acc<string>();
+            acc1.Input();
+            Console.WriteLine(acc1.GetInfo());
+
+            Acc<string> acc2 = new Acc<string>();
+            acc2.Input();
+            Console.WriteLine(acc2.GetInfo());
+
+
+
+            Console.ReadKey();
+        }
+
+
+        //Console.WriteLine("Введите числовой номер");
+        //int kodInt = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Введите имя");
+        //string name =Console.ReadLine(); 
+        //Console.WriteLine("Введите цену");
+        //int price = Convert.ToInt32(Console.ReadLine());
+
+        //Tovar<int> tovar1 = new Tovar<int>(kodInt, name, price);
+
+        //Console.WriteLine(tovar1.GetInfo());
+
+        //Console.WriteLine("Введите строковый номер");
+        //string kodString = Console.ReadLine();
+        //Console.WriteLine("Введите имя");
+        //name = Console.ReadLine();
+        //Console.WriteLine("Введите цену");
+        //price = Convert.ToInt32(Console.ReadLine());
+
+        //Tovar<string> tovar2 = new Tovar<string>(kodString, name, price);
+        //Console.WriteLine(tovar2.GetInfo());
+
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    //Console.WriteLine("Введите числовой номер");
+        //    //int kodInt = Convert.ToInt32(Console.ReadLine());
+        //    //Console.WriteLine("Введите имя");
+        //    //string name =Console.ReadLine(); 
+        //    //Console.WriteLine("Введите цену");
+        //    //int price = Convert.ToInt32(Console.ReadLine());
+
+        //    //Tovar<int> tovar1 = new Tovar<int>(kodInt, name, price);
+
+        //    //Console.WriteLine(tovar1.GetInfo());
+
+        //    //Console.WriteLine("Введите строковый номер");
+        //    //string kodString = Console.ReadLine();
+        //    //Console.WriteLine("Введите имя");
+        //    //name = Console.ReadLine();
+        //    //Console.WriteLine("Введите цену");
+        //    //price = Convert.ToInt32(Console.ReadLine());
+
+        //    //Tovar<string> tovar2 = new Tovar<string>(kodString, name, price);
+        //    //Console.WriteLine(tovar2.GetInfo());
+
+        //    Tovar<int> tovar1 = new Tovar<int>();
+        //    tovar1.Input();
+        //    Console.WriteLine(tovar1.GetInfo());
+
+        //    Tovar<string> tovar2 = new Tovar<string>();
+        //    tovar2.Input();
+        //    Console.WriteLine(tovar2.GetInfo());
+
+
+
+        //    Console.ReadKey();
+        //}
+
+
+    }
+}
