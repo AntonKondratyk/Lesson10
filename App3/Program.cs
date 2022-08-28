b1bfcb6f033ecd8fd92df4feb190ae511d9fac2b
Anton Kondratyk <andeev1910@mail.ru>
2022-08-28 12:19:59 +0500

    111

---
+++ App3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double lenght = Circule.GetLenght(r);
+            double square = Circule.GetSquare(r);
+            string incircule = Circule.GetInCircule(x,y,r);
+            Console.WriteLine($"{lenght} \n{square} \t {incircule}");
+            Console.ReadKey();
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //double square = Rectangle.GetSquare(a, b);
+            //double perimetr = Rectangle.GetPerimetr(a, b);
+            //Console.WriteLine($"{square} \n{perimetr}");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ App3/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    static class Circule
+    {
+        static public double GetLenght(double r)
+        {
+            return 2*r*Math.PI;
+        }
+        static public double GetSquare(double r)
+        {
+            return  r*r*Math.PI;
+        }
+        static public string GetInCircule(double x, double y, double r)
+        {
+            double l = Math.Sqrt(x * x + y * y);
+            if (l>r)
+                return "Не в окружности";
+            return "В окружности";
+        }
+    }
+    //static class Rectangle
+    //{
+    //    static public double GetSquare(double a, double b)
+    //    {
+    //        return a * b;
+    //    }
+    //    static public double GetPerimetr(double a, double b)
+    //    {
+    //        return 2 * (a + b);
+    //    }
+    //}
+}
